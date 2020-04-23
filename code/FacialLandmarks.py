db97e3eeedf711fb92594033771f3c19ea87409b
LucasSchroeder <60207769+LucasSchroeder@users.noreply.github.com>
2020-04-23 00:11:33 -0400

    implemented the facial landmark detector

--- code/FacialLandmarks.py
+++ code/FacialLandmarks.py
@@ -7,15 +7,12 @@ Brown University
 """
 Overview
 ========
-This program uses OpenCV to capture images from the camera, Fourier transform
-them and show the Fourier transformed image alongside the original on the screen.
-
-$ ./liveFFT2.py
+This program uses OpenCV to capture images from the camera
 
 Required: A Python 3.x installation (tested on 3.5.3),
 with: 
     - OpenCV (for camera reading)
-    - numpy, matplotlib, scipy, argparse
+    - numpy, matplotlib, scipy, dlib, imutils
 """
 
 __author__ = "Lucas Schroeder, Christopher Luke, Antohony Peng"
@@ -23,8 +20,17 @@ __contact__ = "lucas_schroeder@brown.edu"
 __date__ = "2020/04/21"
 
 # https://towardsdatascience.com/facial-mapping-landmarks-with-dlib-python-160abcf7d672 
-# Look at this tutorial for inspiration
+# Look at this tutorial
+# You must installing the dependencies by running the following line in your terminal:
+#               pip install numpy opencv-python dlib imutils
+# If this doesn't work, please look at this website and follow the directions
+# https://www.pyimagesearch.com/2018/01/22/install-dlib-easy-complete-guide/ 
 
+# We can train our program to detect different feature points using Dlib. Read more here:
+#  https://medium.com/datadriveninvestor/training-alternative-dlib-shape-predictor-models-using-python-d1d8f8bd9f5c 
+
+from imutils import face_utils
+import dlib
 import cv2 #opencv-based functions
 import time
 import math
@@ -34,12 +40,29 @@ from skimage import io
 from skimage import img_as_float, img_as_ubyte
 from skimage.color import rgb2gray
 
+p = "shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(p)
+
 # if (you have only 1 webcam){ set device = 0} else{ chose your favorite webcam setting device = 1, 2 ,3 ... }
 cap = cv2.VideoCapture(0)
 while True:
-    # Getting our image by webcam and converting it into a gray image scale
+    # Get the image from the webcam and convert it into a gray image scale
     _, image = cap.read()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Get faces into webcam's image
+    rects = detector(gray, 0)
+    
+    # For each detected face, find the landmark.
+    for (i, rect) in enumerate(rects):
+        # Make the prediction and transfom it to numpy array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+    
+        # Draw on our image, all the finded cordinate points (x,y) 
+        for (x, y) in shape:
+            cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
     
     # show the gray image
     cv2.namedWindow('image',cv2.WINDOW_NORMAL)
