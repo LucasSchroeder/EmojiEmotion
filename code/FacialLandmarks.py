63cd2916b3a23ed5acb1d2daaf0e2804ae095e13
chrisluke <44124529+chrisluke@users.noreply.github.com>
2020-04-23 21:58:38 -0400

    working on window

--- code/FacialLandmarks.py
+++ code/FacialLandmarks.py
@@ -44,9 +44,11 @@ p = "shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(p)
 
+run_cam = True
+
 # if (you have only 1 webcam){ set device = 0} else{ chose your favorite webcam setting device = 1, 2 ,3 ... }
-cap = cv2.VideoCapture(0)
-while True:
+cap = cv2.VideoCapture(cv2.CAP_DSHOW)
+while run_cam:
     # Get the image from the webcam and convert it into a gray image scale
     _, image = cap.read()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -72,14 +74,15 @@ while True:
 
     cv2.imshow("image", image)
     #cv2.imshow("Output")
-    
+    print("facce")
     # If you are using a 64-bit machine, you have to modify 
     # cv2.waitKey(0) line as follows : k = cv2.waitKey(0) & 0xFF
-    k = cv2.waitKey(5) & 0xFF
-    
+    k = cv2.waitKey(1) 
+    print("output")
     # This checks if the "esc" key was pressed to exit the livestream
     if k == 27:
         cv2.destroyAllWindows()
+        
         break
     # The next if statement determines if the window was closed using
     # the "x" button in the top left corner of the window. 
@@ -88,7 +91,8 @@ while True:
     # cv::WindowPropertyFlags: getting the flag with index 0 is the fullscreen 
     # property, but actually it doesn't matter which flag to use, they all become
     #  -1 as soon as the window is closed.
-    if cv2.getWindowProperty('image',cv2.WND_PROP_VISIBLE) < 1:        
-        break 
+    #if cv2.getWindowProperty('image',cv2.WND_PROP_VISIBLE) < 1: 
+    #    print("break")       
+    #    break 
 cv2.destroyAllWindows()
 cap.release()
