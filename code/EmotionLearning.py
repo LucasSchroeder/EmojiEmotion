6a3733290f6a3498137d6200464e01f3c65ef804
chrisluke <44124529+chrisluke@users.noreply.github.com>
2020-05-03 23:40:47 -0400

    revised archecture _ keras fix

--- code/EmotionLearning.py
+++ code/EmotionLearning.py
@@ -6,11 +6,11 @@ Brown University
 
 import numpy as np
 import pandas as pd
-# import tensorflow as tf
+import tensorflow as tf
 # #import hyperparameters as hp
 # from tensorflow.keras.layers import \
 #         Conv2D, MaxPool2D, Dropout, Flatten, Dense
-#from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential
 #from tensorflow.keras.optimizers import Adam
 #from tensorflow.keras.losses import categorical_crossentropy
 #from tensorflow.keras.utils import to_categorical
@@ -70,42 +70,42 @@ x_testing = np.divide(np.subtract(x_testing, np.mean(x_testing)), np.std(x_testi
 x_training = x_training.reshape(x_training.shape[0], 48, 48, 1)
 x_testing = x_testing.reshape(x_testing.shape[0], 48, 48, 1)
 
-model = Sequential([
+model = Sequential()
      # Block 1
-    Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1", input_shape=(x_training.shape[1:])),
-    Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"),
-    MaxPool2D(2, name="block1_pool"),
-    tf.keras.layers.Dropout(0.5),
+model.add(Conv2D(64, (3, 3), padding="same", activation="relu", name="block1_conv1", input_shape=(x_training.shape[1:])))
+model.add(Conv2D(64, (3, 3), padding="same", activation="relu", name="block1_conv2"))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2, 2), name="block1_pool"))
+model.add(Dropout(0.5))
     # Block 2
-    Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv1"),
-    Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv2"),
-    MaxPool2D(2, name="block2_pool"),
-    tf.keras.layers.Dropout(0.5),
+model.add(Conv2D(128, (3, 3), padding="same", activation="relu", name="block2_conv1"))
+model.add(Conv2D(128, (3, 3), padding="same", activation="relu", name="block2_conv2"))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2, 2), name="block2_pool"))
+model.add(Dropout(0.5))
     # Block 3, 
-    Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv1"),
-    Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv2"),
-    Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv3"),
-    MaxPool2D(2, name="block3_pool"),
-    tf.keras.layers.Dropout(0.5),
+model.add(Conv2D(256, (3, 3), padding="same", activation="relu", name="block3_conv1"))
+model.add(Conv2D(256, (3, 3), padding="same", activation="relu", name="block3_conv2"))
+model.add(Conv2D(256, (3, 3), padding="same", activation="relu", name="block3_conv3"))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2, 2), name="block3_pool"))
+model.add(Dropout(0.5))
     # Block 4
-    Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv1"),
-    Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv2"),
-    Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv3"),
-    MaxPool2D(2, name="block4_pool"),
-    tf.keras.layers.Dropout(0.2),
+model.add(Conv2D(512, (3, 3), padding="same", activation="relu", name="block4_conv1"))
+model.add(Conv2D(512, (3, 3), padding="same", activation="relu", name="block4_conv2"))
+model.add(Conv2D(512, (3, 3), padding="same", activation="relu", name="block4_conv3"))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2, 2), name="block4_pool"))
+model.add(Dropout(0.2))
     # Block 5
-    Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv1"),
-    Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv2"),
-    Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv3"),
-    MaxPool2D(2, name="block5_pool"),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation= "relu"),
-    tf.keras.layers.Dense(256, activation= "relu"),
-    tf.keras.layers.Dense(128, activation= "relu"),
-    tf.keras.layers.Dense(64, activation= "relu"),
-    tf.keras.layers.Dense(num_labels, activation= "softmax")
-])
+model.add(Conv2D(512, (3, 3), padding="same", activation="relu", name="block5_conv1"))
+model.add(Conv2D(512, (3, 3), padding="same", activation="relu", name="block5_conv2"))
+model.add(Conv2D(512, (3, 3), padding="same", activation="relu", name="block5_conv3"))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2, 2), name="block5_pool"))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(512, activation= "relu"))
+model.add(Dense(256, activation= "relu"))
+model.add(Dense(128, activation= "relu"))
+model.add(Dense(64, activation= "relu"))
+model.add(Dense(num_labels, activation= "softmax"))
+
 """
 model = Sequential()
     
