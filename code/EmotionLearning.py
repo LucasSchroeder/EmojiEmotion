9ac02600ab4d9f5c1aed6c2fc3f4fb851b489af6
LucasSchroeder <60207769+LucasSchroeder@users.noreply.github.com>
2020-05-04 15:58:03 -0400

    made log directory easier to navigate

--- code/EmotionLearning.py
+++ code/EmotionLearning.py
@@ -170,8 +170,8 @@ if not os.path.exists("your_model_checkpoints"):
 
 adam = Adam(lr = hp.learning_rate)
 model.compile(optimizer = adam, loss = 'categorical_crossentropy', metrics = ['accuracy'])
-NAME = 'Emoji-EMotion-cnn-{}'.format(int(time.time()))
-tboard_log_dir = os.path.join("logs",NAME)
+# NAME = 'Emoji-EMotion-cnn-{}'.format(int(time.time()))
+# tboard_log_dir = os.path.join("logs",NAME)
 """callback_list = [
      tf.keras.callbacks.ModelCheckpoint(
             filepath="weights.e{epoch:02d}-" + \
@@ -186,8 +186,10 @@ tboard_log_dir = os.path.join("logs",NAME)
     ] """
     #[ModelCheckpoint("./your_model_checkpoints/weights.hd5",monitor='val_loss', verbose=1, save_best_only=True),TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)]
     #ModelCheckpoint("./your_model_checkpoints/weights.hd5",monitor='val_loss', verbose=1, save_best_only=True),
-callback_list = [ModelCheckpoint(filepath="weights.e{epoch:02d}-" + \
-                    "acc{val_loss:.4f}.h5",monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=True),
+    #ModelCheckpoint(filepath="weights.e{epoch:02d}-" + \
+                   # "acc{val_loss:.4f}.h5",monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=True),
+callback_list = [ModelCheckpoint("your_model_checkpoints/weights.hd5",monitor='val_loss', verbose=1, save_best_only=True,save_weights_only=True),
+                
     # ModelCheckpoint(
     #         filepath="weights.e{epoch:02d}-" + \
     #                 "acc{val_sparse_categorical_accuracy:.4f}.h5",
@@ -197,7 +199,7 @@ callback_list = [ModelCheckpoint(filepath="weights.e{epoch:02d}-" + \
     
         # If you want to visualize the files created during training, run in your terminal
         # tensorboard --logdir path_to_current_dir/Graph 
-        TensorBoard(log_dir=tboard_log_dir, histogram_freq=0, write_graph=True, write_images=True)
+        TensorBoard(log_dir="logs", histogram_freq=0, write_graph=True, write_images=True)
         # TensorBoard(log_dir='Graphs',
         #      update_freq='batch',
         #      profile_batch=0) 
