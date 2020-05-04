4d74f9f55954855d9460501edf3a00eab954b22c
chrisluke <44124529+chrisluke@users.noreply.github.com>
2020-05-03 22:26:27 -0400

    Revert "architecture test pull"
    This reverts commit 92d8a6a358e0f191070aae1415ceb65a1068b7db.

--- code/EmotionLearning.py
+++ code/EmotionLearning.py
@@ -6,14 +6,14 @@ Brown University
 
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+# import tensorflow as tf
 # #import hyperparameters as hp
-from tensorflow.keras.layers import \
-         Conv2D, MaxPool2D, Dropout, Flatten, Dense
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import categorical_crossentropy
-from tensorflow.keras.utils import to_categorical
+# from tensorflow.keras.layers import \
+#         Conv2D, MaxPool2D, Dropout, Flatten, Dense
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.optimizers import Adam
+#from tensorflow.keras.losses import categorical_crossentropy
+#from tensorflow.keras.utils import to_categorical
 
 
 from keras import optimizers
@@ -70,6 +70,7 @@ x_testing = np.divide(np.subtract(x_testing, np.mean(x_testing)), np.std(x_testi
 x_training = x_training.reshape(x_training.shape[0], 48, 48, 1)
 x_testing = x_testing.reshape(x_testing.shape[0], 48, 48, 1)
 
+<<<<<<< HEAD
 optimizer = tf.keras.optimizers.RMSprop(learning_rate= 1e-4, momentum= 0.01)
 
 model = Sequential([
@@ -108,7 +109,97 @@ model = Sequential([
 ])
 
 
-model.compile(loss=categorical_crossentropy, optimizer= optimizer, metrics=['accuracy'])
+model.compile(loss=, optimizer= optimizer, metrics=['accuracy'])
+=======
+# model = Sequential([
+#     # Block 1
+#     Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1", input_shape=(x_training.shape[1:])),
+#     Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"),
+#     MaxPool2D(2, name="block1_pool"),
+#     tf.keras.layers.Dropout(0.2),
+#     # Block 2
+#     Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv1"),
+#     Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv2"),
+#     MaxPool2D(2, name="block2_pool"),
+#     tf.keras.layers.Dropout(0.2),
+#     # Block 3, 
+#     Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv1"),
+#     Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv2"),
+#     Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv3"),
+#     MaxPool2D(2, name="block3_pool"),
+#     tf.keras.layers.Dropout(0.2),
+#     # Block 4
+#     Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv1"),
+#     Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv2"),
+#     Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv3"),
+#     MaxPool2D(2, name="block4_pool"),
+#     tf.keras.layers.Dropout(0.2),
+#     # Block 5
+#     Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv1"),
+#     Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv2"),
+#     Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv3"),
+#     MaxPool2D(2, name="block5_pool"),
+#     tf.keras.layers.Dropout(0.2),
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(256, activation= "relu"),
+#     tf.keras.layers.Dense(64, activation= "relu"),
+#     tf.keras.layers.Dense(num_labels, activation= "softmax")
+# ])
+
+model = Sequential()
+    
+model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(48, 48, 1), kernel_regularizer=l2(0.01)))
+model.add(Conv2D(64, (3, 3), padding='same',activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
+model.add(Dropout(0.5))
+    
+model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
+    
+model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
+    
+model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
+    
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+
+
+learning_rate = 0.001
+#model.compile(loss = 'categorical_crossentropy', optimizer=Adam(lr= learning_rate), metrics=['accuracy'])
+
+adam = optimizers.Adam(lr = learning_rate)
+model.compile(optimizer = adam, loss = 'categorical_crossentropy', metrics = ['accuracy'])
+>>>>>>> 26bdab7c3e51d1845f11d57f458a4fbbbd787a4a
 """callback_list = [
      tf.keras.callbacks.ModelCheckpoint(
             filepath="weights.e{epoch:02d}-" + \
