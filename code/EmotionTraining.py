47ad0f44d0aa266e25b682f314e0ff51bd8aef00
LucasSchroeder <60207769+LucasSchroeder@users.noreply.github.com>
2020-05-04 18:47:19 -0400

    nothing changed

--- code/EmotionTraining.py
+++ code/EmotionTraining.py
@@ -13,24 +13,8 @@ from keras.utils import np_utils
 
 
 df=pd.read_csv('../../EmojiData/fer2013.csv')
-#dfTrain=np.genfromtxt('data/fer2013.csv', delimiter=',')
-# dfTrain=np.genfromtxt('data/train.csv', delimiter=',', dtype=None)
 
-#print(type(df))
-# print(dfTrain[1])
-# print(dfTrain.shape)
-
-print('yay!')
-
-
-# print(df.info())
-# print(df["Usage"].value_counts())
-
-# print(df.head())
 X_train,train_y,X_test,test_y=[],[],[],[]
-
-
-#print("df shape: ",df)
 
 for index, row in df.iterrows():
     val=row['pixels'].split(" ")
@@ -43,8 +27,6 @@ for index, row in df.iterrows():
            test_y.append(row['emotion'])
     except:
         print(f"error occured at index :{index} and row:{row}")
-
-
 
 
 num_features = 64
@@ -62,8 +44,6 @@ test_y = np.array(test_y,'float32')
 train_y=np_utils.to_categorical(train_y, num_classes=num_labels)
 test_y=np_utils.to_categorical(test_y, num_classes=num_labels)
 
-#cannot produce
-#normalizing data between oand 1
 X_train -= np.mean(X_train, axis=0)
 X_train /= np.std(X_train, axis=0)
 
@@ -74,30 +54,23 @@ X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
 
 X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
 
-# print(f"shape:{X_train.shape}")
-##designing the cnn
-#1st convolution layer
 model = Sequential()
 
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(X_train.shape[1:])))
 model.add(Conv2D(64,kernel_size= (3, 3), activation='relu'))
-# model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
 model.add(Dropout(0.5))
 
 #2nd convolution layer
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
 model.add(Dropout(0.5))
 
 #3rd convolution layer
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(Conv2D(128, (3, 3), activation='relu'))
-# model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
-
 model.add(Flatten())
 
 #fully connected neural networks
@@ -107,8 +80,6 @@ model.add(Dense(1024, activation='relu'))
 model.add(Dropout(0.2))
 
 model.add(Dense(num_labels, activation='softmax'))
-
-# model.summary()
 
 #Compliling the model
 model.compile(loss=categorical_crossentropy,
@@ -122,7 +93,6 @@ model.fit(X_train, train_y,
           verbose=1,
           validation_data=(X_test, test_y),
           shuffle=True)
-
 
 #Saving the  model to  use it later on
 fer_json = model.to_json()
